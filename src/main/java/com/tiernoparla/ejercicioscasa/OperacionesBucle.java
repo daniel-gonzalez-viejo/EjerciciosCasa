150e0fd47daabf5422162a231059901fce445962
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-09-29 01:34:24 +0200

    añado operaciones bucle con 'for'

---
+++ src/main/java/com/tiernoparla/ejercicioscasa/OperacionesBucle.java
@@ -0,0 +1,18 @@
+package com.tiernoparla.ejercicioscasa;
+
+
+public class OperacionesBucle {
+    public static void main(String[] args) {
+        
+        
+        
+        for (int a = 4; a < 100; a=a+6){
+            if (a%2 == 0){
+                System.out.println(a);
+                
+            } // if
+            
+        } //for
+            
+    } //main
+} //all
