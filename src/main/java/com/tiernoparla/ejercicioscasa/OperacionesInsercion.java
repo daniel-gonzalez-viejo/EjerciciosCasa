9f6675688ee2b1db141b157132932b2e56ab21fc
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-09-28 02:05:00 +0200

    Ejercición suma numeros dado por teclado

--- src/main/java/com/tiernoparla/ejercicioscasa/OperacionesInsercion.java
+++ src/main/java/com/tiernoparla/ejercicioscasa/OperacionesInsercion.java
@@ -1,6 +1,26 @@
 package com.tiernoparla.ejercicioscasa;
 
+import java.util.Scanner; //Para poder usar la inserción de numeros por teclado
 
 public class OperacionesInsercion {
+    public static void main(String[] args) {
+        
+        Scanner reader = new Scanner (System.in); //Para crear la clase leer, tipo Scanner
+        
+        int a;
+        int b;
+        
+        
+        System.out.println("Introduce el primer numero:");
+        a = reader.nextInt();
+        
+        System.out.println("Introduce el segundo numero:");
+        b = reader.nextInt();
+        
+        int c = a+b;
+        
+        System.out.println("La suma de " + a + " + " + b + " = " + c );
     
-}
+    } //main
+    
+} //OperacionesInsercion
